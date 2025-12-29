ca6ec1ac30debc5bc1432ead5a93b0e8004e6fef
frosb1official-lang <frosb1official@gmail.com>
2025-12-29 21:01:32 +0700

    Search bar filtering has been configured. Layout has been added.

--- abito-app/src/components/Card/Card.jsx
+++ abito-app/src/components/Card/Card.jsx
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom'
 import './Card.css'
 
-export const Card = ({title, price, address, date, img}) => {
+export const Card = ({id, title, price, address, date, img}) => {
     return (
-        <a href="/product.html" className="card">
+        <Link to={`/product/${id}`} className="card">
 
             <div className="card--img">
                 <img src={img} alt="card-img" />
@@ -18,6 +19,6 @@ export const Card = ({title, price, address, date, img}) => {
                 <span className="card--desc">{address}</span>
                 <span className="card--desc">{date}</span>
             </div>
-        </a>
+        </Link>
     )
 }
--- abito-app/src/pages/Home.jsx
+++ abito-app/src/pages/Home.jsx
@@ -1,85 +1,65 @@
-import { Header } from "../components/header/header"
+import { useOutletContext } from "react-router-dom"
 import { Card } from "../components/Card/Card"
-import { cardArray } from "../constants"
 import { Side } from "../components/Side/Side"
-import { sideArray } from "../constants"
+import { cardArray, sideArray } from "../constants"
 
 export const Home = () => {
-    return (
-        <>
-            <Header />
+    const {products} = useOutletContext()
 
-            <main>
-                <section className="search">
-                    <div className="container">
-                        <div className="search-box">
-                            <input type="text" />
-                            <button className="btn btn-primary search-btn">
-                                <img className="search-btn__icon" src="/images/Search.svg" alt="search" />
-                                <span className="search-btn__text">Найти</span>
-                            </button>
+    return (
+        <section className="content">
+            <div className="container">
+                <div className="content-box">
+                    <div className="content-main">
+
+                        <h2 className="content-main__title">Рекомендации для вас</h2>
+                        <div className="content-main__list">
+                            {
+                                products.map(card => (
+                                    <Card 
+                                        key={card.id}
+                                        id={card.id}
+                                        title={card.title}
+                                        price={card.price}
+                                        address={card.address}
+                                        date={card.date}
+                                        img={card.img}
+                                    />
+                                ))
+                            }
                         </div>
                     </div>
-                </section>
-            </main>
+                    <div className="content-side">
+                        <h3 className="content-side__title">Сервисы и услуги</h3>
 
-            <main>
-                <section className="content">
-                    <div className="container">
-                        <div className="content-box">
-                            <div className="content-main">
+                            {
+                                sideArray.map(side => (
+                                    <Side 
+                                        key={side.id}
+                                        img={side.img}
+                                        title={side.title}
+                                        text={side.text}
+                                    />
+                                ))    
+                            }
+                            <hr />
 
-                                <h2 className="content-main__title">Рекомендации для вас</h2>
-                                <div className="content-main__list">
-                                    {
-                                        cardArray.map(card => (
-                                            <Card 
-                                                key={card.id}
-                                                title={card.title}
-                                                price={card.price}
-                                                address={card.address}
-                                                date={card.date}
-                                                img={card.img}
-                                            />
-                                        ))
-                                    }
-                                </div>
-                            </div>
-                            <div className="content-side">
-                                <h3 className="content-side__title">Сервисы и услуги</h3>
+                            <div className="content-side__footer">
+                                <p className="content-side__footer--item">
+                                    © ООО «Абито», 2011–2021
+                                </p>
+                                <a href="#!" className="content-side__footer--item">
+                                    Политика конфиденциальности
+                                </a>
 
-                                    {
-                                        sideArray.map(side => (
-                                            <Side 
-                                                key={side.id}
-                                                img={side.img}
-                                                title={side.title}
-                                                text={side.text}
-                                            />
+                                <a href="#!" className="content-side__footer--item">
+                                    Обработка данных
+                                </a>
 
-                                        ))    
-                                    }
-                                    <hr>
-                                    </hr>
-
-                                    <div className="content-side__footer">
-                                        <p className="content-side__footer--item">
-                                            © ООО «Абито», 2011–2021
-                                        </p>
-                                        <a href="#!" className="content-side__footer--item">
-                                            Политика конфиденциальности
-                                        </a>
-
-                                        <a href="#!" className="content-side__footer--item">
-                                            Обработка данных
-                                        </a>
-
-                                    </div>
-                                </div>
                             </div>
                         </div>
-                </section>
-            </main>
-        </>
+                    </div>
+                </div>
+        </section>
     )
 }
